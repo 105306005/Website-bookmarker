11f8340e7002a6b2a98632098e4e60eb0029d279
maggiesun <maggie20080103@gmail.com>
2021-06-01 01:16:22 -0400

    save bookmark to local storage

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById("website-name");
 const websiteUrlEl = document.getElementById("website-url");
 const bookmarksContainer = document.getElementById("bookmarks-container");
 
+let bookmarks = [];
+
 // Show Modal, Focus on Input
 function showModel() {
   modal.classList.add("show-modal");
@@ -41,6 +43,24 @@ function validate(nameValue, urlValue) {
   return true;
 }
 
+//Fetch Bookmarks
+function fetchBookmarks() {
+  //Get Bookmarks from local storage if available
+  if (localStorage.getItem("bookmarks")) {
+    bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  } else {
+    //Create a bookmarks array in local storage
+    bookmarks = [
+      {
+        name: "youtube",
+        url: "https://youtube.com",
+      },
+    ];
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }
+  console.log(bookmarks);
+}
+
 //Handle data from Form
 function storeBookmark(e) {
   e.preventDefault();
@@ -49,11 +69,22 @@ function storeBookmark(e) {
   if (!urlValue.includes("http://", "https://")) {
     urlValue = `https://${urlValue}`;
   }
-  console.log(nameValue, urlValue);
   if (!validate(nameValue, urlValue)) {
     return false;
   }
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+  bookmarks.push(bookmark);
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  fetchBookmarks();
+  bookmarkForm.reset();
+  websiteNameEl.focus();
 }
 
 //Event Listeners
 bookmarkForm.addEventListener("submit", storeBookmark);
+
+//On load - fetch Bookmarks
+fetchBookmarks();
